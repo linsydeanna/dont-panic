c920348e8a799611335cbb54a752668080cbe0c1
Linsy <Linsy.Joyner@gmail.com>
2016-12-18 14:26:04 -0500

    posted incidents to database

--- src/components/severity.js
+++ src/components/severity.js
@@ -3,12 +3,41 @@ import {Link} from 'react-router';
 import * as actionCreators from '../actions/actionCreators'
 import { bindActionCreators } from 'redux';
 import AddSeverityForm from '../components/addSeverityForm';
+import axios from 'axios';
 
 class Severity extends Component {
 
+  postIncident() {
+    console.log("postIncident");
+    console.log("severity is ", this.props.incidents.severity);
+    console.log("duration is ", this.props.incidents.duration);
+    console.log("symptoms is ", this.props.incidents.symptoms);
+    axios.post('https://pure-mesa-82885.herokuapp.com/users/ljoyner/incidents', {
+      'severity': this.props.incidents.severity,
+      'duration': this.props.incidents.duration,
+      'symptoms': this.props.incidents.symptoms,
+      'username': 'ljoyner'
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+    axios.get('https://pure-mesa-82885.herokuapp.com/users/ljoyner/incidents')
+    .then((response) => {
+  console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
   render() {
 
-    const { dispatch } = this.props;
+    const { dispatch, incidents } = this.props;
+    console.log("this.props.incidents is ", incidents);
     const addSeverity = bindActionCreators(actionCreators.addSeverity, dispatch);
 
     return (
@@ -20,6 +49,7 @@ class Severity extends Component {
           <p>Finish</p>
         </Link>
       </div>
+      <button onClick={() => this.postIncident()}>Post Data</button>
       </div>
     );
   }
