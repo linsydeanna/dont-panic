43d3b23add688ea4b89d4392f1c0b30c43449b35
Linsy <Linsy.Joyner@gmail.com>
2017-01-01 23:48:34 -0500

    added modal and spinner

--- src/components/login.js
+++ src/components/login.js
@@ -1,34 +1,68 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router'
 import '../styles/App.css';
+import '../styles/login.css';
+import Modal from 'react-modal';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      modalIsOpen: false
+    }
+  }
+
+  openModal() {
+    this.setState({
+      modalIsOpen: true
+    })
+  }
+
+  closeModal() {
+    this.setState({
+      modalIsOpen: false
+    })
+  }
 
   render() {
     return (
+      <div>
+        <button onClick={() => this.openModal()}>Log in</button>
+      <Modal isOpen={this.state.modalIsOpen} contentLabel="modal">
       <div className="LoginBox">
         <div className="LoginForm">
           <div className="LoginText">
-            <p>First name</p>
+            <p>FIRST NAME</p>
             <input className="LoginInput"/>
           </div>
           <div className="LoginText">
-            <p>Last name</p>
+            <p>LAST NAME</p>
             <input className="LoginInput"/>
           </div>
           <div className="LoginText">
-            <p>Email</p>
+            <p>USERNAME</p>
             <input className="LoginInput"/>
           </div>
           <div className="LoginText">
-            <p>Password</p>
+            <p>EMAIL ADDRESS</p>
             <input className="LoginInput"/>
           </div>
-          <div className="SegmentLink">
-            <Link to="/start">
-              <p>Login</p></Link>
+          <div className="LoginText">
+            <p>PASSWORD</p>
+            <input className="LoginInput"/>
+          </div>
+          <div className="LoginText">
+            <p>CONFIRM PASSWORD</p>
+            <input className="LoginInput"/>
           </div>
         </div>
+        <div className="SegmentLink">
+          <Link to="/start">
+            <p>Log in</p>
+          </Link>
+        </div>
+      </div>
+      </Modal>
       </div>
     );
   }
--- src/components/reports.js
+++ src/components/reports.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Record from './record';
 import axios from 'axios';
+import '../styles/spinner.css';
 
 class Reports extends Component {
   constructor() {
@@ -37,12 +38,23 @@ class Reports extends Component {
    .catch(function (response) {
       console.error(response);
    });
-
   }
-
   render() {
     if (this.state.loading) {
-      return <div>Loading Reports</div>;
+      return (<div className="sk-fading-circle">
+        <div className="sk-circle1 sk-circle"></div>
+        <div className="sk-circle2 sk-circle"></div>
+        <div className="sk-circle3 sk-circle"></div>
+        <div className="sk-circle4 sk-circle"></div>
+        <div className="sk-circle5 sk-circle"></div>
+        <div className="sk-circle6 sk-circle"></div>
+        <div className="sk-circle7 sk-circle"></div>
+        <div className="sk-circle8 sk-circle"></div>
+        <div className="sk-circle9 sk-circle"></div>
+        <div className="sk-circle10 sk-circle"></div>
+        <div className="sk-circle11 sk-circle"></div>
+        <div className="sk-circle12 sk-circle"></div>
+      </div>)
     }
     return (
       <div>
