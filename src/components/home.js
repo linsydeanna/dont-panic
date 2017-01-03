c1cfdb58d657d92c5895da2f68cf29e2d46e57cd
Linsy <Linsy.Joyner@gmail.com>
2017-01-02 22:36:10 -0500

    styled login page

--- src/components/home.js
+++ src/components/home.js
@@ -9,7 +9,7 @@ class Home extends Component {
     return (
       <div className="Home">
         <div className="Nav">
-          <img className="Logo" src={Logo} alt="logo"/>
+          <img className="LogoNav" src={Logo} alt="logo"/>
           <Link className="NavItem" to="/start">
             <p>Home</p>
           </Link>
--- src/components/login.js
+++ src/components/login.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router'
 import '../styles/App.css';
 import '../styles/login.css';
 import Modal from 'react-modal';
+import Logo from '../logo.png';
 
 class Login extends Component {
   constructor() {
@@ -26,43 +27,51 @@ class Login extends Component {
 
   render() {
     return (
-      <div>
-        <button onClick={() => this.openModal()}>Log in</button>
-      <Modal isOpen={this.state.modalIsOpen} contentLabel="modal">
-      <div className="LoginBox">
-        <div className="LoginForm">
-          <div className="LoginText">
-            <p>FIRST NAME</p>
-            <input className="LoginInput"/>
+      <div className="LoginPage">
+        <img className="Logo" src={Logo} alt="logo"/>
+        <div className="LoginButtonContainer">
+          <div className="SegmentLink" onClick={() => this.openModal()}>
+            <p>Get started</p>
           </div>
-          <div className="LoginText">
-            <p>LAST NAME</p>
-            <input className="LoginInput"/>
-          </div>
-          <div className="LoginText">
-            <p>USERNAME</p>
-            <input className="LoginInput"/>
-          </div>
-          <div className="LoginText">
-            <p>EMAIL ADDRESS</p>
-            <input className="LoginInput"/>
-          </div>
-          <div className="LoginText">
-            <p>PASSWORD</p>
-            <input className="LoginInput"/>
-          </div>
-          <div className="LoginText">
-            <p>CONFIRM PASSWORD</p>
-            <input className="LoginInput"/>
-          </div>
-        </div>
-        <div className="SegmentLink">
-          <Link to="/start">
+          <div className="SegmentLink" onClick={() => this.openModalTwo()}>
             <p>Log in</p>
-          </Link>
+          </div>
         </div>
-      </div>
-      </Modal>
+        <Modal isOpen={this.state.modalIsOpen} contentLabel="modal"
+          className="LoginBox">
+          <div onClick={() => this.closeModal()}>X</div>
+          <div className="LoginForm">
+            <div className="LoginText">
+              <p>FIRST NAME</p>
+              <input className="LoginInput"/>
+            </div>
+            <div className="LoginText">
+              <p>LAST NAME</p>
+              <input className="LoginInput"/>
+            </div>
+            <div className="LoginText">
+              <p>USERNAME</p>
+              <input className="LoginInput"/>
+            </div>
+            <div className="LoginText">
+              <p>EMAIL ADDRESS</p>
+              <input className="LoginInput"/>
+            </div>
+            <div className="LoginText">
+              <p>PASSWORD</p>
+              <input className="LoginInput"/>
+            </div>
+            <div className="LoginText">
+              <p>CONFIRM PASSWORD</p>
+              <input className="LoginInput"/>
+            </div>
+          </div>
+          <div className="SegmentLink">
+            <Link to="/start">
+              <p>Log in</p>
+            </Link>
+          </div>
+        </Modal>
       </div>
     );
   }
